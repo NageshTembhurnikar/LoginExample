f7210f019bd1beeebe7bad5ac6a1a74dd0de3942
Nagesh Tembhurnikar <nagesh@introp.net>
2014-02-19 11:01:31 +0530

    Login Page with ui view controller created

---
+++ LoginExample/LoginPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginPageViewController.h
+//  LoginExample
+//
+//  Created by Nagesh on 2/19/14.
+//  Copyright (c) 2014 nagesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginPageViewController : UIViewController
+
+@end
---
+++ LoginExample/LoginPageViewController.m
@@ -0,0 +1,38 @@
+//
+//  LoginPageViewController.m
+//  LoginExample
+//
+//  Created by Nagesh on 2/19/14.
+//  Copyright (c) 2014 nagesh. All rights reserved.
+//
+
+#import "LoginPageViewController.h"
+
+@interface LoginPageViewController ()
+
+@end
+
+@implementation LoginPageViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
