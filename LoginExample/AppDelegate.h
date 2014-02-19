57bbc7236137634f407440bba5ea13685ddecbf4
Nagesh Tembhurnikar <nagesh@introp.net>
2014-02-19 10:39:41 +0530

    Initial Commit

---
+++ LoginExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LoginExample
+//
+//  Created by Nagesh on 2/19/14.
+//  Copyright (c) 2014 nagesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LoginExampleTests/LoginExampleTests.m
@@ -0,0 +1,34 @@
+//
+//  LoginExampleTests.m
+//  LoginExampleTests
+//
+//  Created by Nagesh on 2/19/14.
+//  Copyright (c) 2014 nagesh. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LoginExampleTests : XCTestCase
+
+@end
+
+@implementation LoginExampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
